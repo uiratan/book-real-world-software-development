81a1ce3c46e9efb50c1b0a173fb54fda69b7508d
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-25 15:23:53 -0300

    initial commit

--- src/main/java/com/rwsd/Application.java
+++
@@ -1,8 +0,0 @@
-package com.rwsd;
-
-public class Application {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/com/rwsd/chapter02/Application.java
@@ -0,0 +1,11 @@
+package com.rwsd.chapter02;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // What is the total profit and loss from a list of bank statements? Is it positive or negative?
+        // How many bank transactions are there in a particular month?
+        // What are his top-10 expenses? 
+        // Which category does he spend most of his money on?
+    }
+}
