5c22366a76829e952d807c253867138fccbcc068
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-25 16:07:09 -0300

    God class

---
+++ src/main/java/com/rwsd/chapter02/BankTransactionAnalyzerProblematic.java
@@ -0,0 +1,47 @@
+package com.rwsd.chapter02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankTransactionAnalyzerProblematic {
+
+    private static final String RESOURCES = "src/main/resources/";
+    private static final  DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public static void main(String... args) throws IOException {
+        final Path path = Paths.get(RESOURCES + "bank-statements.csv");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0;
+
+        // 1 - What is the total profit and loss from a list of bank statements? 
+        // Is it positive or negative?
+        for(final String line: lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        } 
+        
+        System.out.println("The total for all transactions is " + total);
+
+        // How many bank transactions are there in a particular month?
+        total = 0;
+        
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_FORMATTER);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+
+        System.out.println("The total for all transactions in January is " + total);
+    }
+            
+}
